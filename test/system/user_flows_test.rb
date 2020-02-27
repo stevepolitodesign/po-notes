0d75b3cfe8a10167994659377af220a83ab94024
Steve Polito <info@stevepolitodesign.com>
2020-02-27 06:16:20 -0500

    create pending user flow tests

---
+++ test/system/user_flows_test.rb
@@ -0,0 +1,19 @@
+require "application_system_test_case"
+
+class UserFlowsTest < ApplicationSystemTestCase
+  test "logging in" do
+    skip
+  end
+
+  test "logging out" do
+    skip
+  end
+
+  test "updating account" do
+    skip
+  end
+  
+  test "signing up" do
+    skip
+  end
+end
