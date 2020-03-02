41bc9a60af427f02a6dd0b462c2323c2ba461c0e
Steve Polito <info@stevepolitodesign.com>
2020-03-02 06:31:38 -0500

    add pending user flow integration tests

---
+++ test/integration/user_flows_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UserFlowsTest < ActionDispatch::IntegrationTest
+  test "can register" do
+    skip
+  end
+
+  test "can sign in" do
+    skip
+  end
+  
+  test "can sign out" do
+    skip
+  end
+
+  test "can cancel account" do
+    skip
+  end
+end
