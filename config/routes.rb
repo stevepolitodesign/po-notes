6746f5c29a350a12cdfd5becb6e4c5f78dc275db
Steve Polito <info@stevepolitodesign.com>
2020-03-02 06:06:50 -0500

    rails generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/dashboard'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root 'pages#dashboard', as: :authenticated_root
+  end
+
+  root "pages#home"
 end
