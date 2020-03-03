7aebb536dad777e2ad447a9e3270e9b57310a946
Steve Polito <info@stevepolitodesign.com>
2020-03-03 07:26:58 -0500

    add assertions to registration test

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class UserFlowsTest < ActionDispatch::IntegrationTest
   test "can register" do
-    skip
+    get new_user_registration_path
+    assert_select '#new_user' do
+      assert_select 'input#user_email'
+      assert_select 'input#user_password'
+      assert_select 'input#user_password_confirmation'
+    end
   end
 
   test "can sign in" do
