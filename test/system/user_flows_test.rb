53cf624c140229344026107ab117650c4d2f57aa
Steve Polito <info@stevepolitodesign.com>
2020-03-03 10:05:42 -0500

    write user registration test

--- test/system/user_flows_test.rb
+++ test/system/user_flows_test.rb
@@ -1,19 +1,33 @@
 require "application_system_test_case"
 
 class UserFlowsTest < ApplicationSystemTestCase
-  test "logging in" do
-    skip
+  include ActionMailer::TestHelper
+
+  test "can register" do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'user_1@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    assert_emails 1 do
+      click_on 'Sign up'
+    end
+    page.find('body', text: 'A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.')
   end
 
-  test "logging out" do
-    skip
-  end
-
-  test "updating account" do
+  test "can sign in" do
     skip
   end
   
-  test "signing up" do
+  test "can sign out" do
     skip
   end
+
+  test "can cancel account" do
+    skip
+  end  
+
+  test "can update account" do
+    skip
+  end
+
 end
