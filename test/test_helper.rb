89acde9bdb88abf9eabba28682f3627a4f3ca72a
stevepolitodesign <info@stevepolitodesign.com>
2020-03-04 11:01:34 -0500

    disable paper_trail from tests
    https://github.com/paper-trail-gem/paper_trail#7a-minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,18 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # https://github.com/paper-trail-gem/paper_trail#7a-minitest
+  def with_versioning
+    was_enabled = PaperTrail.enabled?
+    was_enabled_for_request = PaperTrail.request.enabled?
+    PaperTrail.enabled = true
+    PaperTrail.request.enabled = true
+    begin
+      yield
+    ensure
+      PaperTrail.enabled = was_enabled
+      PaperTrail.request.enabled = was_enabled_for_request
+    end
+  end
 end
