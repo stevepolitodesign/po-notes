44d3a67c3d20d434dea57f7ea968ce353dc0603a
Steve Polito <info@stevepolitodesign.com>
2020-03-24 19:40:52 -0400

    Only load clipboard script if copy button exists.

--- app/javascript/packs/clipboard.js
+++ app/javascript/packs/clipboard.js
@@ -1,5 +1,6 @@
-import ClipboardJS from 'clipboard';
+import ClipboardJS from "clipboard";
 
 document.addEventListener("turbolinks:load", () => {
-    new ClipboardJS('.js-copy-to-clipboard');
+  const clipBoardButton = document.querySelector(".js-copy-to-clipboard");
+  clipBoardButton && new ClipboardJS(clipBoardButton);
 });
