3920537e281e200e4dcbc189723b20199aa71268
Steve Polito <info@stevepolitodesign.com>
2020-05-06 06:52:10 -0400

    Add default values to columns.

--- db/migrate/20200506105038_create_plans.rb
+++ db/migrate/20200506105038_create_plans.rb
@@ -1,10 +1,10 @@
 class CreatePlans < ActiveRecord::Migration[6.0]
   def change
     create_table :plans do |t|
-      t.string :name
-      t.integer :notes_limit
-      t.integer :tasks_limit
-      t.integer :reminders_limit
+      t.string :name, null: false
+      t.integer :notes_limit, null: false, default: 500
+      t.integer :tasks_limit, null: false, default: 100
+      t.integer :reminders_limit, null: false, default: 25
 
       t.timestamps
     end
