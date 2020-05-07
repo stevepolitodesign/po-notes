25a30a6bfb4af14353c71475d873fcfe8f9c86e2
Steve Polito <info@stevepolitodesign.com>
2020-05-07 09:22:50 -0400

    Add default plan to user table.

--- db/migrate/20200506135059_add_plan_model_to_users.rb
+++ db/migrate/20200506135059_add_plan_model_to_users.rb
@@ -2,6 +2,6 @@ class AddPlanModelToUsers < ActiveRecord::Migration[6.0]
   disable_ddl_transaction!
 
   def change
-    add_reference :users, :plan, null: false, index: {algorithm: :concurrently}
+    add_reference :users, :plan, null: false, default: 1, index: {algorithm: :concurrently}
   end
 end
