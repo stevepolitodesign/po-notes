a6ee8fb56414571af5962d524a21bfa13ba4d3cc
Steve Polito <info@stevepolitodesign.com>
2020-05-15 15:22:54 -0400

    Write failing tests to help guide me building the reminder interface.

---
+++ test/system/reminder_flows_test.rb
@@ -0,0 +1,35 @@
+require "application_system_test_case"
+
+class ReminderFlowsTest < ApplicationSystemTestCase
+  test "should display a list of reminders" do
+    flunk
+  end
+
+  test "should have no result bahavior" do
+    flunk
+  end
+
+  test "should alert user if they do not have a phone number" do
+    flunk
+  end
+
+  test "should create a reminder" do
+    flunk
+  end
+
+  test "should update a reminder" do
+    flunk
+  end
+
+  test "should destroy a reminder" do
+    flunk
+  end
+
+  test "should display user's time_zone on form" do
+    flunk
+  end
+
+  test "should display errors during validation" do
+    flunk
+  end
+end
