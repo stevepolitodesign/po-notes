a24dd7223cf2056d32749d0c8bd8acacd08dc162
Steve Polito <info@stevepolitodesign.com>
2020-05-18 17:47:22 -0400

    Add default_url_options.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = {host: "po-notes.com"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
