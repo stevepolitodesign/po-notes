61461f06c1aefb229a6b34fddfdcffb795a55d75
Steve Polito <info@stevepolitodesign.com>
2020-05-21 15:39:47 -0400

    Create base for note_imports controller.

--- app/controllers/note_imports_controller.rb
+++ app/controllers/note_imports_controller.rb
@@ -1,2 +1,10 @@
 class NoteImportsController < ApplicationController
+    before_action :authenticate_user!
+
+    def new
+    end
+
+    def create
+        redirect_to notes_path, notice: "Importing notes. Check back later."
+    end
 end
--- test/controllers/note_imports_controller_test.rb
+++ test/controllers/note_imports_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class NoteImportsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = users(:user_1)
+  end
+
+  test "should post to create if authenticated" do
+    sign_in @user
+    post import_notes_path, params: {file: "notes.csv"}
+    assert_redirected_to notes_path
+  end
+
+  test "should not post to create if anonymous" do
+    post import_notes_path, params: {file: "notes.csv"}
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get new if authenticated" do
+    sign_in @user
+    get import_notes_path
+    assert_response :success
+  end
+
+  test "should not get new if anonymous" do
+    get import_notes_path
+    assert_redirected_to new_user_session_path
+  end
 end
