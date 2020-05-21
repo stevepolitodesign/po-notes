f77702e5317ebcd57071e631efebbddb8164382d
Steve Polito <info@stevepolitodesign.com>
2020-05-21 15:53:23 -0400

    Create simple note import job.

--- app/jobs/import_note_job.rb
+++ app/jobs/import_note_job.rb
@@ -1,7 +1,8 @@
 class ImportNoteJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(note, user)
+    note.user = user
+    note.save
   end
 end
--- test/jobs/import_note_job_test.rb
+++ test/jobs/import_note_job_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ImportNoteJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:user_1)
+    @note = Note.new(title: "A new note", body: "Some text")
+    @note.tag_list.add("tag 1", "tag 2")
+  end
+
+  test "should create a new note" do
+    assert_difference("Note.count") do
+      ImportNoteJob.perform_now(@note, @user)
+    end
+  end
+  
 end
