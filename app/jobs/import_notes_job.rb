f6e350795da1acafa74717bb8153608904715e92
Steve Polito <info@stevepolitodesign.com>
2020-05-23 13:57:58 -0400

    Add smoke test for import_notes_job

--- app/jobs/import_notes_job.rb
+++ app/jobs/import_notes_job.rb
@@ -3,15 +3,11 @@ require "csv"
 class ImportNotesJob < ApplicationJob
   queue_as :default
 
-  def perform(file, user)
-  end
-
-  private 
-
-  # TODO Should only parse as many row's as the user's plan's notes_limit
-  def parse_csv
-  end
-
-  def save_note
+  def perform(file:, user:, limit: nil)
+    CSV.parse(file, headers: true, header_converters: :symbol) do |row|
+      @note = Note.new(title: row[:title], body: row[:body], user: user)
+      @note.tag_list.add(row[:tags].split(",")) if row[:tags]
+      @note.save if @note.valid?
+    end
   end
 end
--- test/jobs/import_notes_job_test.rb
+++ test/jobs/import_notes_job_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class ImportNotesJobTest < ActiveJob::TestCase
   def setup
     @user = users(:user_1)
-    @file
+    @file = file_fixture("notes.csv").read
   end
 
   test "should import notes" do
     assert_difference("Note.count") do
-      ImportNotesJob.perform_now(@file, @user)
+      ImportNotesJob.perform_now(file: @file, user: @user)
     end
   end
   
