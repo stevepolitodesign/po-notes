141e3318f69a615ae649c362d39b483e061918b1
Steve Polito <info@stevepolitodesign.com>
2020-05-23 14:28:41 -0400

    Limit imported notes.

--- app/jobs/import_notes_job.rb
+++ app/jobs/import_notes_job.rb
@@ -1,10 +1,14 @@
-
 require "csv"
 class ImportNotesJob < ApplicationJob
   queue_as :default
 
   def perform(file:, user:, limit: nil)
-    CSV.parse(file, headers: true, header_converters: :symbol) do |row|
+    rows = if limit
+      CSV.parse(file, headers: true, header_converters: :symbol).take(limit)
+    else
+      CSV.parse(file, headers: true, header_converters: :symbol)
+    end
+    rows.each do |row|
       @note = Note.new(title: row[:title], body: row[:body], user: user)
       @note.tag_list.add(row[:tags].split(",")) if row[:tags]
       @note.save if @note.valid?
--- test/jobs/import_notes_job_test.rb
+++ test/jobs/import_notes_job_test.rb
@@ -1,15 +1,20 @@
-require 'test_helper'
-
+require "test_helper"
+require "csv"
 class ImportNotesJobTest < ActiveJob::TestCase
   def setup
     @user = users(:user_1)
-    @file = file_fixture("notes.csv").read
+    @file = file_fixture("notes.csv")
   end
 
   test "should import notes" do
-    assert_difference("Note.count") do
-      ImportNotesJob.perform_now(file: @file, user: @user)
+    assert_difference("Note.count", @file.readlines.size - 1) do
+      ImportNotesJob.perform_now(file: @file.read, user: @user)
     end
   end
-  
+
+  test "should limit created notes" do
+    assert_difference("Note.count", 1) do
+      ImportNotesJob.perform_now(file: @file.read, user: @user, limit: 1)
+    end
+  end
 end
