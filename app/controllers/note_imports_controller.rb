cd552d6230403ca90896571d8ae76c4e83c4dab7
Steve Polito <info@stevepolitodesign.com>
2020-05-23 16:40:19 -0400

    Refactor controller and perform job now.

--- app/controllers/note_imports_controller.rb
+++ app/controllers/note_imports_controller.rb
@@ -5,12 +5,21 @@ class NoteImportsController < ApplicationController
   end
 
   def create
-    if params[:file] && File.extname(params[:file]) == ".csv"
-      limit = current_user.plan.notes_limit if current_user.try(:plan) && current_user.plan.respond_to?(:notes_limit)
-      ImportNotesJob.perform_later(file: params[:file].path, user: current_user, limit: limit)
+    if is_valid_file?
+      ImportNotesJob.perform_now(file: params[:file].path, user: current_user, limit: import_limit)
       redirect_to notes_path, notice: "Importing notes. Check back later."
     else
       redirect_to import_notes_path, notice: "Please upload a CSV."
     end
+  end
+
+  private 
+
+  def import_limit
+    current_user.try(:plan).try(:notes_limit)
+  end
+
+  def is_valid_file?
+    params[:file] && File.extname(params[:file]) == ".csv"
   end
 end
