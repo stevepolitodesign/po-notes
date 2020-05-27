dc00032fdced8b5ba4feeb9764eb6906da437a36
Steve Polito <info@stevepolitodesign.com>
2020-05-27 17:25:53 -0400

    Add basic export functionality.

--- app/controllers/note_exports_controller.rb
+++ app/controllers/note_exports_controller.rb
@@ -1,2 +1,8 @@
+require 'csv'
 class NoteExportsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        @notes = current_user.notes
+    end
 end
--- test/controllers/note_exports_controller_test.rb
+++ test/controllers/note_exports_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class NoteExportsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = users(:user_1)
+  end
+
+  test "should get index if authenticated" do
+    sign_in @user
+    get export_notes_path
+    assert_response :success
+  end
+
+  test "should repond with a csv" do
+    sign_in @user
+    get export_notes_path
+    assert @response.request.format.csv?
+  end
+
+  test "should not get index if anonymous" do
+    get export_notes_path
+    assert_redirected_to new_user_session_path
+  end
+
 end
