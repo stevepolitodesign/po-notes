ad450e823819288eec6b990e5a0e6ff08372f992
Steve Polito <info@stevepolitodesign.com>
2020-05-30 11:29:35 -0400

    Load SimpleMDE styles.

--- app/javascript/packs/simplemde.js
+++ app/javascript/packs/simplemde.js
@@ -1,4 +1,5 @@
 import SimpleMDE from "simplemde";
+import "simplemde/dist/simplemde.min.css";
 
 document.addEventListener("turbolinks:load", () => {
   const element = document.querySelector(".js-simplemde");
