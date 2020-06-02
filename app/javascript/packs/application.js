1948ba8d625e8406c93a06d7126bed9b0eda0e9d
Steve Polito Design <stevepolitodesign@users.noreply.github.com>
2020-06-02 06:47:49 -0400

    Place vendor scrips in vendor directory. (#43)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,8 +15,8 @@ require("channels");
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require("./tagify");
-require("./clipboard");
-require("./simplemde");
+require("../vendor/tagify");
+require("../vendor/clipboard");
+require("../vendor/simplemde");
 
 import "controllers";
