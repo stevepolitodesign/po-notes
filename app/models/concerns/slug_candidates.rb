4be1e15552a50b785dbe08ad3b29935084f051e9
Steve Polito <info@stevepolitodesign.com>
2020-06-01 20:33:41 -0400

    Extract slug candidate logic into slug_candidates concern.

--- app/models/concerns/slug_candidates.rb
+++ app/models/concerns/slug_candidates.rb
@@ -1,7 +1,10 @@
 module SlugCandidates
   extend ActiveSupport::Concern
-
+  
   included do
+    extend FriendlyId
+    friendly_id :slug_candidates, use: :slugged
+
     private
 
     def slug_candidates
