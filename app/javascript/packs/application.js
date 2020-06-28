e514b6614ec9171f622a8cddd6967f1af71dd444
Steve Polito <info@stevepolitodesign.com>
2020-06-28 15:41:58 -0400

    Toggle user profile links.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
+require("alpinejs");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -22,4 +23,3 @@ require("../vendor/simplemde");
 require("../vendor/fontawesome");
 import "controllers";
 
-import 'alpinejs';
